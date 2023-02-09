b748476f30fbbca8741e63314fed28dc13f72dff
muxyku <muxykush@gmail.com>
2023-02-09 21:19:12 +0800

    merged with previous pull, create users and login/logout

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import transformers
 import pandas as pd
 import numpy as np
-import spacy
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
@@ -9,41 +8,44 @@ from sklearn.pipeline import Pipeline
 import tensorflow as tf
 from keras_preprocessing.sequence import pad_sequences
 
-from spacy.lang.en.stop_words import STOP_WORDS
 
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, Response
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, Response, flash
 from flask_mysqldb import MySQL
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from keras.models import load_model
 import MySQLdb.cursors
 import re
 import cv2
 from PIL import Image
 import time
+import MySQLdb.cursors
 
-import numpy as np
-import tensorflow as tf
-
-import cv2
-from PIL import Image
-import time
+from wtforms import StringField, PasswordField, BooleanField, RadioField
+from wtforms.validators import InputRequired, Email, Length, Optional
 
 import numpy as np
 import tensorflow as tf
 
 app = Flask(__name__)
 
-app.secret_key = 'secret'
+#app.secret_key = 'secret'
  
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'root'
+#app.config['MYSQL_PASSWORD'] = 'password'
+#app.config['MYSQL_DB'] = 'classupdb'
+ 
+app.config['MYSQL_HOST'] = 'classupdb.cgdsnk6an6d3.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'bYaP6tsnsRFy1TIJVQAr'
 app.config['MYSQL_DB'] = 'classupdb'
- 
  
 mysql = MySQL(app)
 hand_model = load_model('models/HandGestureModel.h5')
 sentimental_model = load_model('models/sentiment_model.h5', custom_objects={"TFBertModel": transformers.TFBertModel})
+
+
 
 def preprocess_input_data(sentence):
     tokenizer = transformers.BertTokenizer.from_pretrained("bert-base-cased")
@@ -56,10 +58,10 @@ def preprocess_input_data(sentence):
 
 @app.route("/")
 def home():
-    return render_template(
-        "index.html"
-    )
-
+    if 'loggedin' in session:
+        return render_template("index.html")
+    return redirect(url_for('login'))
+    
 @app.route("/slides")
 def slides():
     return render_template(
@@ -106,61 +108,163 @@ def student_index():
 def reflection():
     return render_template('student/sentiment_reflection.html')
 
-def gen_frames():  
+
+@app.route("/slides_list")
+def slides_list():
+    return render_template('slides_list.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                if user.role == "admin":
+                    session["email"] = user.email
+                    session["username"] = user.username
+                    return redirect(url_for('viewAllUsers'))
+                else:
+                    session["email"] = user.email
+                    session["username"] = user.username
+                    return redirect(url_for('consumerUpdateUser'))
+            else:
+                flash("Incorrect Username or password")
+
+                return redirect('/login')
+        else:
+            flash("Incorrect Username or Password")
+            return redirect('/login')
+
+    return render_template('login.html', form=form, user=current_user)
+
+@app.route("/account")
+def account():
+    return render_template('account.html')
+
+
+# def gen_frames():  
+#     video = cv2.VideoCapture(0)
+#     while True:
+#         success, frame = video.read()
+#         if not success:
+#             break
+#         else:
+#             ret, buffer = cv2.imencode('.jpg', frame)
+#             frame = buffer.tobytes()
+#             yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
+
+
+# @app.route("/hand_video_feed")
+# def hand_video_feed():
+#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+global COMMAND
+COMMAND = None
+
+def controlSlides():
     video = cv2.VideoCapture(0)
+    
     while True:
         success, frame = video.read()
         if not success:
             break
+
         else:
+            img = Image.fromarray(frame, 'RGB')
             ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+            
+            frame_tobytes = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_tobytes + b'\r\n')
 
+            img = img.resize((128,128))
+            img_array = np.array(img)
+            
+            img_array = img_array.reshape(1,128,128,3)
 
-@app.route("/hand_video_feed")
-def hand_video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+            prediction = hand_model.predict(img_array)
+            print(prediction)
+            
+            if(prediction[0][0] > 0.998):
+                direction = "left"
+                # response = {
+                #     'command': 'back'
+                # }
+                time.sleep(5)
+                #return jsonify(response)
 
+            elif(prediction[0][0] < 0.5):
+                direction = "right"
+                # response = {
+                #     'command': 'next'
+                # }
+                time.sleep(5)
+                #return jsonify(response)
 
-@app.route("/controlSlides", methods=['POST'])
-def controlSlides():
-    video = cv2.VideoCapture(0)
+            else:
+                direction = "none"
+                time.sleep(1)
 
-    while True:
-        success, frame = video.read()
-
-        img = Image.fromarray(frame, 'RGB')
-
-        img = img.resize((128,128))
-        img_array = np.array(img)
-        
-        img_array = img_array.reshape(1,128,128,3)
-
-        prediction = hand_model.predict(img_array)
-        print(prediction)
-        
-        if(prediction[0][0] == 1 and prediction[0][1] == 0):
-            direction = "left"
+            
             print(direction)
-            time.sleep(3)
-
-        elif(prediction[0][0] < 0.5):
-            direction = "right"
-            print(direction)
-            time.sleep(3)
-
-        else:
-            direction = "none"
-            print(direction)
-            time.sleep(3)
-
-        #return jsonify({'result': direction})
-        
+            
 
         cv2.imshow("Prediction", frame)
         cv2.waitKey(1)
+    #fetch("https://aap-dewmify-classup-image.ayftbvf4bbhqbudp.southeastasia.azurecontainer.io/command",{
+    #   method: "POST",
+    #   headers: {
+    #       "Content-Type": "application/json"     
+    #   },
+    #   body: {
+    #       'command': COMMAND
+    #   }
+    # })
+    #.then(response => response.json())
+    #.then(result => {
+    #   alert(result.result);
+    # })
+
+@app.route("/controlSlides_feed")
+def controlSlides_feed():
+    return Response(controlSlides(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route("/command/", methods={"POST"})
+def command():
+    global COMMAND
+    #fetch("https://aap-dewmify-classup-image.ayftbvf4bbhqbudp.southeastasia.azurecontainer.io/command",{
+    #   method: "POST",
+    #   headers: {
+    #       "Content-Type": "application/json"     
+    #   },
+    #   body: {
+    #       'command': COMMAND
+    #   }
+    # })
+    #.then(response => response.json())
+    #.then(result => {
+    #   alert(result.result);
+    # })
+    returnvalue = jsonify({'command': COMMAND})
+    COMMAND = None
+    
+    return (returnvalue)
+
+@app.route("/setcmdnext/")
+def setcmdnext():
+    COMMAND = 'next'
+    return jsonify({'command': COMMAND})
+    
+@app.route("/setcmdback/")
+def setcmdback():
+    COMMAND = 'back'
+    return jsonify({'command': COMMAND})
 
 @app.route("/prediction", methods=["POST"])
 def prediction():
---
+++ ppt-remote.py
@@ -0,0 +1,56 @@
+from win32gui import GetWindowText, GetForegroundWindow
+import time, requests, sys, getopt, keyboard
+
+REQUESTS_CONNECT_TIMEOUT    = 5
+REQUESTS_READ_TIMEOUT       = 5
+
+COMMANDS = ['next', 'back', 'stop']
+
+def getcommand (p_server):
+    try:
+        r = requests.get('http://' + p_server + '/command', timeout=(REQUESTS_CONNECT_TIMEOUT, REQUESTS_READ_TIMEOUT))
+    except:
+        print('WARNING: unable to access command server')
+        return(None)
+        
+    if r.status_code != requests.codes.ok:
+        return (None)
+        
+    rjson = r.json()
+    
+    if not rjson is None:
+        rcmd = rjson['command']
+        if rcmd in COMMANDS:
+            return(rcmd)
+    
+    return(None)
+
+
+def main(argv):
+    #initialize variables for command line arguments
+    arg_server = '127.0.0.1:8080'
+
+    #get command line arguments
+    try:
+        opts, args = getopt.getopt(argv, 's:')
+    except getopt.GetoptError:
+        sys.exit(2)
+        
+    for opt, arg in opts:
+        if opt == '-s':
+            arg_server = arg
+
+    print ('INFO: Polling server %s' % arg_server)        
+            
+    while (True):
+        time.sleep(1)
+        wintext = GetWindowText(GetForegroundWindow())
+        cmd = getcommand (arg_server)
+        if not cmd is None:
+            if   cmd == 'next':
+                keyboard.send('right')
+            elif cmd == 'back':
+                keyboard.send('left')
+            
+if __name__ == '__main__':
+    main(sys.argv[1:])
